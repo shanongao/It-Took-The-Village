81051de18f136937f772c26ab0f86d21108d2e28
Greg <greg.neil.frantzen@gmail.com>
2023-10-15 23:48:11 +1300

    try to add tutorial npc, run up to it and press e

---
+++ Assets/Scripts/PlayerTutorialInteraction.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class PlayerTutorialInteraction : MonoBehaviour
+{
+    private bool isInTutorialZone = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("TutorialNPC"))
+        {
+            isInTutorialZone = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("TutorialNPC"))
+        {
+            isInTutorialZone = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (isInTutorialZone && Input.GetKeyDown(KeyCode.E))
+        {
+            DisplayTutorialInstructions();
+        }
+    }
+
+    private void DisplayTutorialInstructions()
+    {
+        Debug.Log("Find your sword! Press space to kill enemies.");
+    }
+}
